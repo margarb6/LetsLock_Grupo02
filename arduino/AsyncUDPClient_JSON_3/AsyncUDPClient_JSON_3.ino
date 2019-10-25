9ed370aa6899525a4846c10d8a3e1a4ecfdda341
Sergio <sergiopujmar3@gmail.com>
2019-10-25 03:35:47 +0200

    Final Version Arduino S1

--- arduino/AsyncUDPClient_JSON_3/AsyncUDPClient_JSON_3.ino
+++ arduino/AsyncUDPClient_JSON_3/AsyncUDPClient_JSON_3.ino
@@ -2,14 +2,24 @@
 #include "AsyncUDP.h"
 #include <TimeLib.h>
 #include <ArduinoJson.h>
+#include "time.h"
+
+
+const int EchoPin = 26;
+const int TriggerPin = 25;
+const int sensor = 2;              // the pin that the sensor is atteched to
+int state = LOW;             // by default, no motion detected
+int val = 0;
+StaticJsonDocument<200> jsonBuffer;
 const char * ssid = "Team_2";
 const char * password = "12345678"; //contraseña
 AsyncUDP udp;
-StaticJsonDocument<200> jsonBuffer; //tamaño maximo de los datos
 void setup()
 {
-  Serial.begin(115200);
-  setTime (9, 15, 0, 7, 10, 2018); //hora minuto segundo dia mes año
+  Serial.begin(9600);
+  pinMode(TriggerPin, OUTPUT);
+  pinMode(EchoPin, INPUT);
+  pinMode(sensor, INPUT);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   if (WiFi.waitForConnectResult() != WL_CONNECTED) {
@@ -26,16 +36,52 @@ void setup()
       Serial.println();
     });
   }
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
 }
 void loop()
-{
+{ //enviar datos
+  // SENSOR ULTRASÓNICO
   delay(1000);
   char texto[200];
-  jsonBuffer["Hora"] = hour(); //Datos introducidos en el objeto “jsonbuffer"
-  jsonBuffer["Minuto"] = minute(); //3 campos
-  jsonBuffer["Segundo"] = second(); //
+  
+  // SENSOR PIR
+  int comprobar = 0;
+  for (int i = 0; i < 300; i++) {
+    val = digitalRead(sensor);   // read sensor value
+    if (val == HIGH) {
+      comprobar++;
+    }
+    //delay(10);
+  }
+  
+  
+  //delay(1000);
+  int dist = distancia(TriggerPin, EchoPin);
+  jsonBuffer["Distancia"] = dist;
+  jsonBuffer["Comprobar"] = comprobar;
   serializeJson(jsonBuffer, texto); //paso del objeto “jsonbuffer" a texto para
-  //transmitirlo
-  udp.broadcastTo(texto, 2222); //se envía por el puerto 1234 el JSON
-  //como texto
+  udp.broadcastTo(texto, 2222);
+  /*delay(1000);
+    char texto[200];
+    jsonBuffer["Hora"] = hour(); //Datos introducidos en el objeto “jsonbuffer"
+    jsonBuffer["Minuto"] = minute(); //3 campos
+    jsonBuffer["Segundo"] = second(); //
+    serializeJson(jsonBuffer, texto); //paso del objeto “jsonbuffer" a texto para
+    //transmitirlo
+    udp.broadcastTo(texto, 2222); //se envía por el puerto 1234 el JSON
+    //como texto
+  */
+}
+
+
+int distancia(int TriggerPin, int EchoPin) {
+  long duracion, distanciaCm;
+  digitalWrite(TriggerPin, LOW); //nos aseguramos señal baja al principio
+  delayMicroseconds(4);
+  digitalWrite(TriggerPin, HIGH); //generamos pulso de 10us
+  delayMicroseconds(10);
+  digitalWrite(TriggerPin, LOW);
+  duracion = pulseIn(EchoPin, HIGH); //medimos el tiempo del pulso
+  distanciaCm = duracion * 10 / 292 / 2; //convertimos a distancia
+  return distanciaCm;
 }
--- arduino/Medidor_ultrasonido_Arduino/Medidor_ultrasonido_Arduino.ino
+++ arduino/Medidor_ultrasonido_Arduino/Medidor_ultrasonido_Arduino.ino
@@ -1,8 +1,10 @@
 #include <M5Stack.h>
 
-
 const int EchoPin = 26;
 const int TriggerPin = 25;
+const int sensor = 2;              // the pin that the sensor is atteched to
+int state = LOW;             // by default, no motion detected
+int val = 0;
 
 void setup() {
   M5.begin();
@@ -10,6 +12,7 @@ void setup() {
   Serial.begin(9600);
   pinMode(TriggerPin, OUTPUT);
   pinMode(EchoPin, INPUT);
+  pinMode(sensor, INPUT);
 }
 /*void loop() {
   Serial.print("Distancia: ");
@@ -17,19 +20,41 @@ void setup() {
   delay(1000);
   }*/
 void loop() {
-  
+  M5.Lcd.fillScreen(BLACK);
   if (Serial.available() > 0) {
-    
+
     char command = (char) Serial.read(); switch (command) {
       case 'H':
-        Serial.println("Hola Mundo"); 
+        Serial.println("Hola Mundo");
         M5.Lcd.print("Hola Mundo");
         break;
-        
+
       case 'D':
-        Serial.println(distancia(TriggerPin, EchoPin)); 
+        Serial.println(distancia(TriggerPin, EchoPin));
+        M5.Lcd.println("----------Ult-----------");
         M5.Lcd.print("Distancia: ");
-        M5.Lcd.print(distancia(TriggerPin, EchoPin));
+        M5.Lcd.println(distancia(TriggerPin, EchoPin));
+        break;
+        
+      case 'P':
+        int comprobar = 0;
+        for (int i = 0; i < 300; i++) {
+          val = digitalRead(sensor);   // read sensor value
+          if (val == HIGH) {
+            comprobar++;
+          }
+          delay(30);
+        }
+        
+        if (comprobar > 0) {
+          Serial.print("La habitacion esta ocupada");
+          M5.Lcd.println("-----------PIR-----------");
+          M5.Lcd.println("La habitacion esta ocupada");
+        } else {
+          Serial.print("No hay gente en la habitacion");
+          M5.Lcd.println("-----------PIR-----------");
+          M5.Lcd.println("No hay gente en la habitacion");
+        }
         break;
     }
   }
