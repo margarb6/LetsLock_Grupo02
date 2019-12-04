ad047e6f3608eca6af8b018b63f65fae0454c8c2
Fabio <fabioherlop@gmail.com>
2019-12-04 23:18:52 +0100

    Animacion noton puerta 2

--- app/LetsLock/app/src/main/java/es/upv/gnd/letslock/Fragments/InicioFragment.java
+++ app/LetsLock/app/src/main/java/es/upv/gnd/letslock/Fragments/InicioFragment.java
@@ -1,5 +1,12 @@
 package es.upv.gnd.letslock.Fragments;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Gravity;
@@ -13,6 +20,12 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieProperty;
+import com.airbnb.lottie.model.KeyPath;
+import com.airbnb.lottie.value.LottieFrameInfo;
+import com.airbnb.lottie.value.SimpleLottieValueCallback;
+
 import es.upv.gnd.letslock.R;
 
 public class InicioFragment extends Fragment {
@@ -21,13 +34,39 @@ public class InicioFragment extends Fragment {
     ImageView imageView;
     boolean image1Displaying = true;
     Toast toast;
+    LottieAnimationView lottieAnimationView;
 
+    @SuppressLint("ClickableViewAccessibility")
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         vista = inflater.inflate(R.layout.fragment_inicio, container, false);
 
 
-        imageView = vista.findViewById(R.id.candado);
+        imageView = vista.findViewById(R.id.puerta);
+        lottieAnimationView = vista.findViewById(R.id.animation_view);
+        lottieAnimationView.setSpeed((float) 0.5);
+
+        lottieAnimationView.addAnimatorListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                lottieAnimationView.pauseAnimation();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
 
         imageView.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -37,18 +76,32 @@ public class InicioFragment extends Fragment {
                    @Override
                    public void run() {
                        if (image1Displaying){
-                           imageView.setImageResource(R.drawable.tick_app);
-
-                           toast.makeText(getActivity(), "Puerta abierta", Toast.LENGTH_SHORT).show();
+                           toast.makeText(getActivity(), "Abriendo puerta", Toast.LENGTH_SHORT).show();
+                           imageView.setImageResource(R.drawable.imagen_animacion);
+                           lottieAnimationView.playAnimation();
                            image1Displaying = false;
                        }else{
-                           imageView.setImageResource(R.drawable.candado_app);
-                           toast.makeText(getActivity(), "Puerta cerrada", Toast.LENGTH_SHORT).show();
+                           imageView.setImageResource(R.drawable.imagen_animacion);
+                           float progress = lottieAnimationView.getProgress();
+                           int duration = 3100;
+                           ValueAnimator valueAnimator = ValueAnimator.ofFloat(-progress,0 )
+                                   .setDuration(duration);
+
+                           valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                               @Override
+                               public void onAnimationUpdate(ValueAnimator animation) {
+
+                                   lottieAnimationView.setProgress(Math.abs((float)animation.getAnimatedValue()));
+                               }
+
+                           });
+                           valueAnimator.start();
+                           toast.makeText(getActivity(), "Cerrando puerta", Toast.LENGTH_SHORT).show();
                            image1Displaying = true;
                        }
 
                    }
-               },2000);
+               },0);
 
                 return false;
             }
